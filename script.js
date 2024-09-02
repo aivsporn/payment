86b5e0c92502750a627f1076bbaa417323e4be63
aivsporn <aivsporn@gmail.com>
2024-09-02 14:36:27 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -8,16 +8,32 @@ document.addEventListener('DOMContentLoaded', displayUsername);
 
 async function createOrder(){
     let username = new URL(window.location.href).searchParams.get("username");
-    let url = `${baseUrl}?action=createPayment&username=${username}`
+    let password = new URL(window.location.href).searchParams.get("password");
+    let url = `${baseUrl}?action=createPayment&username=${username}&password=${password}`
     let call = new XMLHttpRequest();
     call.open("GET", url, false);
     call.send();
     return call.responseText;
 }
+
 async function onApprove(data, actions){
     // use data.orderID to capture the order
+    let url = `${baseUrl}?action=confirmPayment&orderId=${data.orderID}`;
+    let call = new XMLHttpRequest();
+    call.open("GET", url, false);
+    call.send();
+    let status = call.responseText;
     // return actions.restart() for "INSTRUMENT_DECLINED" 
-    // no need to return anything for successful orders
+    if(status == "INSTRUMENT_DECLINED"){
+        document.getElementById("paymentStatus").innerHTML = status + ". Please try again, or contact aivsporn@gmail.com for additional support.";
+        return actions.restart();
+    }
+    else if(status == "success"){
+        document.getElementById("paymentStatus").innerHTML = "complete. Open the AI vs. Porn extension to see your new credit balance, and start browsing a porn-free internet!";
+    }
+    else{
+        document.getElementById("paymentStatus").innerHTML = status + ". Please contact aivsporn@gmail.com for additional support. Sorry for the inconvenience.";
+    }
 }
 
 window.paypal.Buttons({
